a0bcc440f6fad0f2345a4e9683df07b014854724
hanxiaobo <xiaobo.han@zyprotect.com>
2021-03-01 15:28:05 +0800

    readme

--- README.md
+++ README.md
@@ -30,3 +30,60 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 根目录下执行: npm run packagr
 进入dist目录下执行： npm pack
 登录npm，执行：npm publish
+
+## 使用
+html:
+<zy-editable-table [id]="'test'"
+[config]="tableConfig"
+[lang]="tableConfigLang"
+[data]="data"
+(outer)="insetData($event)"></zy-editable-table>
+
+ts:
+tableConfigLang = {
+    operating: '操作',
+    reset: '重置',
+    up: '向上出入一条',
+    down: '向下插入一条',
+    delete: '删除',
+    formerror: '格式错误'
+  }
+  tableConfig = [
+    {
+      name: 'ip', key: 'ip', type: 'text', search: true,
+      validators: function (val) {
+        var str = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
+        return str.test(val)
+      }
+    },
+    {
+      name: '开关', key: 'show', type: 'boolean',
+      filters: [{ name: 'open', value: true }, { name: 'close', value: false }]
+    },
+    {
+      name: '选择', key: 'CaseSensitive', type: 'select',
+      filters: [
+        { name: 'Any Field Type', value: '' },
+        { name: 'Checkbox', value: 'checkbox' },
+        { name: 'Hidden', value: 'hidden' },
+        { name: 'Radio', value: 'radio' },
+        { name: 'Select', value: 'select' },
+        { name: 'Text', value: 'text' },
+        { name: 'Textarea', value: 'textarea' }
+      ]
+    }];
+  data = [{ ip: "1.1.1.1", show: true, CaseSensitive: "checkbox" }];
+
+  insetData($event) {
+    let data = JSON.parse($event)
+    console.log(data)
+    if (data.type === 'up') {
+      console.log('向上插入')
+    } else if (data.type === 'down') {
+      console.log('向下插入')
+    } else if (data.type === 'delete') {
+      console.log('删除')
+    } else if (data.type === 'edit') {
+      console.log('编辑')
+    }
+  }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,31 +12,46 @@ export class AppComponent {
     reset: '重置',
     up: '向上出入一条',
     down: '向下插入一条',
-    delete: '删除'
+    delete: '删除',
+    formerror: '格式错误'
   }
   tableConfig = [
-    { name: 'Path', key: 'Path', type: 'text', search: true },
     {
-      name: '开关', key: 'SecureOnly', type: 'boolean',
+      name: 'ip', key: 'ip', type: 'text', search: true,
+      validators: function (val) {
+        var str = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
+        return str.test(val)
+      }
+    },
+    {
+      name: '开关', key: 'show', type: 'boolean',
       filters: [{ name: 'open', value: true }, { name: 'close', value: false }]
     },
     {
       name: '选择', key: 'CaseSensitive', type: 'select',
-      filters: [{ name: 'Any Field Type', value: '' }, { name: 'Checkbox', value: 'checkbox' }, { name: 'Hidden', value: 'hidden' }, { name: 'Radio', value: 'radio' }, { name: 'Select', value: 'select' }, { name: 'Text', value: 'text' }, { name: 'Textarea', value: 'textarea' }]
+      filters: [
+        { name: 'Any Field Type', value: '' },
+        { name: 'Checkbox', value: 'checkbox' },
+        { name: 'Hidden', value: 'hidden' },
+        { name: 'Radio', value: 'radio' },
+        { name: 'Select', value: 'select' },
+        { name: 'Text', value: 'text' },
+        { name: 'Textarea', value: 'textarea' }
+      ]
     }];
-  data = [{Path: "^\/URI$", SecureOnly: true, CaseSensitive: "checkbox"}];
+  data = [{ ip: "1.1.1.1", show: true, CaseSensitive: "checkbox" }];
 
   insetData($event) {
-		let data = JSON.parse($event)
+    let data = JSON.parse($event)
     console.log(data)
-		if (data.type === 'up') {
-			console.log('向上插入')
-		} else if (data.type === 'down') {
-			console.log('向下插入')
-		} else if (data.type === 'delete') {
-			console.log('删除')
-		} else if (data.type === 'edit') {
-			console.log('编辑')
-		}
-	}
+    if (data.type === 'up') {
+      console.log('向上插入')
+    } else if (data.type === 'down') {
+      console.log('向下插入')
+    } else if (data.type === 'delete') {
+      console.log('删除')
+    } else if (data.type === 'edit') {
+      console.log('编辑')
+    }
+  }
 }
